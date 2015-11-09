6c0f76a4299159aa778a120774eaca6f5b677ecb
Loraine <loraine@lorainekv.com>
2015-11-09 10:10:27 -0800

    Created all endpoints.

--- video_store/routes.py
+++ video_store/routes.py
@@ -5,12 +5,38 @@ from video_store import app
 def index():
     return "Video Store API!"
 
-@app.route('/')
+# ____ CUSTOMER ENDPOINTS ____
+
+# GET all customers
+# /customers
 @app.route('/customers/')
 def customers():
     return "List of customers!"
 
-@app.route('/')
-@app.route('/customers/<int:id>')
-def customer(id):
-    return "A customer's profile!"
+# GET subset of customers sorted by name, registered_at, or postal_code
+# /customers/name/page1
+@app.route('/customers/<column>/page<int:page>')
+def customers_subset(column, page):
+    return "Subset of customers on page {}!".format(page)
+
+
+# ____ MOVIES ENDPOINTS ____
+
+# GET all movies
+# /movies
+@app.route('/movies/')
+def movies():
+    return "List of movies!"
+
+# GET subset of movies sorted by title or release_date
+# /movies/name/page1
+@app.route('/movies/<column>/page<int:page>')
+def movies_subset(column, page):
+    return "Subset of movies on page {}!".format(page)
+
+# ____ RENTALS ENDPOINTS ____
+
+# GET rental info on a specific movie
+@app.route('/rentals/<movie_title>')
+def rental_movie(movie_title):
+    return "List of rental movies!"
