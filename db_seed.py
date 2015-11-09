a99a30f17ae3382ec2b240e395ffbd6b5b05d0bf
Loraine <loraine@lorainekv.com>
2015-11-09 12:59:49 -0800

    WIP seed script.

---
+++ db_seed.py
@@ -0,0 +1,4 @@
+from video_store.models import Customer
+from video_store import db
+
+test = models.Customer(1, 'name', 'registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit')
