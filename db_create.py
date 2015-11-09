86eb2586ef1c5551921cf874c0ef9f5b0c65b5d9
Loraine <loraine@lorainekv.com>
2015-11-09 15:32:13 -0800

    Creating the movie model and table.

--- db_create.py
+++ db_create.py
@@ -1,10 +1,14 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
+import os.path
+
 from video_store import db
 from video_store.models import Customer
-import os.path
+from video_store.models import Movie
+
 db.create_all()
+
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
--- video_store/models.py
+++ video_store/models.py
@@ -24,3 +24,19 @@ class Customer(db.Model):
 
     def __repr__(self):
         return '<Customer %r>' % (self.name)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    overview = db.Column(db.String(200))
+    release_date = db.Column(db.String(200))
+    inventory = db.Column(db.Integer)
+
+    def __init__(self, title, overview, release_date, inventory):
+        self.title = title
+        self.overview = overview
+        self.release_date = release_date
+        self.inventory = inventory
+
+    def __repr__(self):
+        return '<Movie %r>' % (self.name)
